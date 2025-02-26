485255e91aeef53f930d6068c1987c8c2e9d5313
Suraj Sachan <100021123+Surajsachan29889@users.noreply.github.com>
2025-02-26 19:23:33 +0530

    Update configuration files and Dockerfile for deployment

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM node:20.10.0-alpine
 WORKDIR /usr/src/app
 COPY package*.json ./
 RUN npm install
+RUN npx prisma migrate dev --name init
 COPY . .
-EXPOSE 3000
+EXPOSE 5001
 CMD ["npm", "start"]
--- src/index.ts
+++ src/index.ts
@@ -20,5 +20,5 @@ app.use("/api/v1/auth", authRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
